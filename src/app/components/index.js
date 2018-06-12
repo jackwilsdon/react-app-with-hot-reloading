4303f1be072fab4e2608912665e321bc8ab17fd9
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-12 19:55:35 +0100

    Restructure application

---
+++ src/app/components/index.js
@@ -0,0 +1,4 @@
+export { default as App } from './App';
+
+// We don't export HotApp here, as exporting it causes hot module reload to fail!
+// see: https://github.com/gaearon/react-hot-loader/blob/master/docs/Troubleshooting.md#not-all-of-my-code-got-updated
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HotApp from './HotApp';
+import HotApp from './app/components/HotApp';
 
 ReactDOM.render(<HotApp />, document.getElementById('root'));
