7f6dd1fa9ae98cc4f14144ee07ecfdd94906bb11
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-12 17:25:52 +0100

    Enable hot reloading

---
+++ src/HotApp.js
@@ -0,0 +1,4 @@
+import App from './App';
+import { hot } from 'react-hot-loader';
+
+export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import HotApp from './HotApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HotApp />, document.getElementById('root'));
