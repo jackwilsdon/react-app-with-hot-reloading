930e2c3921b05156ed29b965db7806dba62c8a4e
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-13 20:18:27 +0100

    Add snapshot test for Counter component

---
+++ src/components/Counter/Counter.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Counter } from './Counter';
+import { createStore } from 'redux';
+import noop from 'lodash/noop';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Counter onAdd={noop} onSubtract={noop} value={12345} />,
+  );
+
+  expect(tree).toMatchSnapshot();
+});
