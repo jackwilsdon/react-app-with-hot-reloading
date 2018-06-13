cb0a2c51fd573440eae9ff8a43c718381b69e688
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-13 21:55:56 +0100

    Add stylelint for linting CSS

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,19 @@
 const rewireEslint = require('react-app-rewire-eslint');
 const rewireHotLoader = require('react-app-rewire-hot-loader');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 const flow = require('lodash/flow');
+
+const rewireStyleLint = (config, env) => {
+  return env === 'production'
+    ? config
+    : {
+        ...config,
+        plugins: [...(config.plugins || []), new StyleLintPlugin()],
+      };
+};
 
 module.exports = flow(
   rewireEslint,
   rewireHotLoader,
+  rewireStyleLint,
 );
