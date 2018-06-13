8d07b6dced4d3ceaafb224d3080cccf5386e7efa
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-13 20:27:00 +0100

    Add ESLint configuration

--- config-overrides.js
+++ config-overrides.js
@@ -1,1 +1,8 @@
-module.exports = require('react-app-rewire-hot-loader');
+const rewireEslint = require('react-app-rewire-eslint');
+const rewireHotLoader = require('react-app-rewire-hot-loader');
+const flow = require('lodash/flow');
+
+module.exports = flow(
+  rewireEslint,
+  rewireHotLoader,
+);
