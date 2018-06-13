2d618104146cb3ebdf84d4d3ec141eaba8e882c3
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-13 19:42:36 +0100

    Simplify createStore in test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
-
-const rootReducer = () => ({});
+import identity from 'lodash/identity';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const store = createStore(rootReducer);
+  const store = createStore(identity, {});
 
   ReactDOM.render(<App store={store} />, div);
   ReactDOM.unmountComponentAtNode(div);
