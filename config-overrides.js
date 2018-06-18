57d053de6cee0d6664b7620a125e2963b4d9bf40
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-18 22:30:46 +0100

    Add custom "flow" function to ensure environment is passed to all rewires

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,13 @@
 const rewireEslint = require('react-app-rewire-eslint');
 const rewireHotLoader = require('react-app-rewire-hot-loader');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
-const flow = require('lodash/flow');
+
+// Custom "flow" function for passing environment to every function.
+const flow = (...functions) => (config, env) =>
+  functions.reduce(
+    (previousConfig, nextFunction) => nextFunction(previousConfig, env),
+    config,
+  );
 
 const rewireStyleLint = (config, env) => {
   return env === 'production'
