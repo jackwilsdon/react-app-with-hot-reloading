86dca14e140f227dd710fe76465cdef9485a3998
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-18 23:03:08 +0100

    Add named mapDispatchToProps argument to connect

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -6,8 +6,9 @@ import { getType } from '../selectors/router';
 const Switch = ({ type }) => React.createElement(components[type] || Fragment);
 
 const mapStateToProps = state => ({ type: getType(state) });
+const mapDispatchToProps = {};
 
 export default connect(
   mapStateToProps,
-  {},
+  mapDispatchToProps,
 )(Switch);
