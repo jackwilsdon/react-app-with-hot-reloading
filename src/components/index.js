927d33eac96abf47dbc5309d18fc7e8ba232ee46
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-19 20:18:03 +0100

    Add named counter component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as App } from './App';
 export { default as Counter } from './Counter';
+export { default as NamedCounter } from './NamedCounter';
 export { default as Switch } from './Switch';
