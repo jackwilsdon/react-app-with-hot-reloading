f243b23a41a6d230c99b51951e0a23e98d7642e3
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-06-20 23:13:15 +0100

    Tidy up jest mocks

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -1,10 +1,6 @@
-const path = require('path');
-
-// This is a custom Jest transformer turning file imports into filenames.
-// http://facebook.github.io/jest/docs/en/webpack.html
+const { basename } = require('path');
 
 module.exports = {
-  process(src, filename) {
-    return `module.exports = ${JSON.stringify(path.basename(filename))};`;
-  },
+  process: (src, filename) =>
+    `module.exports = ${JSON.stringify(basename(filename))};`,
 };
